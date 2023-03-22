2df909aa60e48c7adb533dd54836d1c664adf14a
TheInfamousGrim <finchergeorge1@gmail.com>
2023-03-22 01:01:16 +0000

    Feat 🎯: Build nav bar
    Built out the nav bar.
    
    Added the main color pallette for the site.
    
    Added favicons and site logo to the site.
    
    Added placeholder pictures and data for the categories.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Fira_Sans } from 'next/font/google';
+import Navigation from '@/components/Navigation';
 
 export default function Home() {
   return (
@@ -12,9 +13,27 @@ export default function Home() {
           content="The place to get all your schwifty, phantasmagoric, vaporwave inspired apparel."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="./favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="./favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="./favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="./favicons/site.webmanifest" />
       </Head>
-      <main className="h-screen bg-black">
+      <Navigation />
+      <main className="h-screen bg-amaranth-pink">
         <h2 className="font-newake text-7xl text-white">hello</h2>
         <p className="font-firasans text-6xl text-white">Big boy</p>
         <p className="text-5xl text-white">hey now</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,17 @@ module.exports = {
       fontFamily: {
         'firasans': ["Fira Sans", "sans-serif"],
         'newake': ["Newake", "sans-serif"],
+      },
+      colors: {
+        'amaranth-pink': '#E89CB7',
+        'space-cadet': '#312B5D',
+        crayola: '#FB6A82',
+        aquamarine: "#00FFAB",
+        sunglow: '#FECC4E',
+        'rich-black': '#070013',
+        'floral-white': '#FFFCF2',
+        'dark-violet': '#8B25C9',
+        'bright-pink': '#FC356D'
       }
     },
   },
